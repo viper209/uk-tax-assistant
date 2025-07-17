05bf8894c879a955b9f8ce5b078952ee9c4a9791
viper209 <124597980+viper209@users.noreply.github.com>
2025-07-17 11:13:43 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,12 @@
-content: [
-  './index.html',
-  './src/**/*.{js,jsx,ts,tsx}',
-  './public/**/*.html'
-],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
